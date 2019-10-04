cfeafe88d408eadbf23032237ffeab16533acf9e
VsevolodSur <giwecm@gmail.com>
2019-10-04 08:12:36 +0300

    Commit stepik 4.1 step 6

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose Language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    print("\nStart browser for test..")
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(10)
+    yield browser
+    print("\nQuit browser..")
+    browser.quit()
