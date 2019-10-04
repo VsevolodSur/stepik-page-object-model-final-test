e8f0ee761ac6ea55b2498974dd67914e689e7683
VsevolodSur <giwecm@gmail.com>
2019-10-04 11:51:40 +0300

    stepik 4.2 step 9 Transitions between pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
+from .login_page import LoginPage
 import time
 
 
@@ -8,9 +9,9 @@ class MainPage(BasePage):
 
     def go_to_login_page(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
-        time.sleep(1)
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
