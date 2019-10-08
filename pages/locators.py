3c8edbf312ef68ecb1f886770527cfd623d08700
VsevolodSur <giwecm@gmail.com>
2019-10-08 09:59:19 +0300

    stepik 4.3 step 6 add negative checks

--- pages/locators.py
+++ pages/locators.py
@@ -15,3 +15,4 @@ class ProductPageLocators(object):
     PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main > h1")
     PRODUCT_IN_BACKET_PRICE = (By.CSS_SELECTOR, ".fade.in > div > p:nth-child(1) > strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "div.col-sm-6.product_main > p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alert-success")
--- pages/product_page.py
+++ pages/product_page.py
@@ -29,3 +29,11 @@ class ProductPage(BasePage):
         # print(f'\n{product_in_backet_name.text}\n{product_name.text}')
         assert product_in_backet_price.text == product_price.text, "Product price do not match:("
         time.sleep(1)
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is presented, but should not be"
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message should not be disappeared"
